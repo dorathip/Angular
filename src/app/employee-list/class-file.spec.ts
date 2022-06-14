8a6144fee1f2c115915a283abfcedd270c3712cb
dorathip <107496028+dorathip@users.noreply.github.com>
2022-06-14 12:18:24 -0400

    Add files via upload

---
+++ src/app/employee-list/class-file.spec.ts
@@ -0,0 +1,7 @@
+import { ClassFile } from './class-file';
+
+describe('ClassFile', () => {
+  it('should create an instance', () => {
+    expect(new ClassFile()).toBeTruthy();
+  });
+});
---
+++ src/app/employee-list/class-file.ts
@@ -0,0 +1,15 @@
+export class ClassFile {
+
+    constructor(
+
+        public employeeID: string,
+        public name : string,
+        public salary : number,
+        public designation : string,
+        public credits : number
+
+
+
+    ){}
+
+}
